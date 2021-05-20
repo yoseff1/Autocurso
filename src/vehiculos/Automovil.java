2fa705ef75f1ffb98095dc7213aba18260f7eb35
yoseff <yoseffcajamarca@gmail.com>
2021-05-20 15:59:04 -0300

    clase de string

--- src/vehiculos/Automovil.java
+++ src/vehiculos/Automovil.java
@@ -8,7 +8,23 @@ public class Automovil {
 	private int velocidadActual=0;
 	private boolean encendido;
 	private String patente;
-	public String color;
+	private String color;
+	
+	public Automovil(String marca, String modelo,int velocidadMaxima,String patente,String color) {
+			this.marca = marca;
+			this.modelo = modelo;
+			this.velocidadMaxima = velocidadMaxima;
+			this.patente = patente;
+			this.color =color;
+			}
+	
+	public Automovil(String marca, String modelo,int velocidadMaxima,String patente) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.velocidadMaxima = velocidadMaxima;
+		this.patente = patente;
+		this.color = "gris";
+}
 	
 	public void setMarca(String m) {
 		marca = m;
@@ -19,18 +35,24 @@ public class Automovil {
 	public void acelerar(int velocidad) {
 		velocidadActual = velocidadActual + velocidad;
 	}
+	public void setVelocidadMaxima(int velocidadMaxima) {
+		this.velocidadMaxima = velocidadMaxima;
+	}
 	public int getVelocidadActual() {
 		return velocidadActual;
+	}
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
 	}
 	public void frenar() {
 		// operacion para frenar
 		///guardar
 	}
-	void encender() {
+	public void encender() {
 		//operacion para acelerar
 		//hice cambios en la rama freno
 	}
-	void apagar() {
+	public void apagar() {
 		
 	}
 	
